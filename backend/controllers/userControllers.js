4503136644a91e7d7f92efdc632f01b07056282c
Mezan <meaznurr@gmail.com>
2024-09-16 17:31:12 +0600

    create a middleware auth.js module 6 video no: 1

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -172,4 +172,5 @@ exports.login = async (req, res) =>{
 
 
 
+
 // console.log((+new Date() * Math.random()).toString().substring(0, 1));
--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,9 +1,14 @@
-const express = require("express")
-const router = express.Router()
-const {newUser, verifiedUser, login} = require('../../controllers/userControllers')
+const express = require("express");
+const router = express.Router();
+const {
+  newUser,
+  verifiedUser,
+  login,
+} = require("../../controllers/userControllers");
+const { authUser } = require("../../middleware/auth");
 
-router.post('/', newUser)
-router.post('/activate', verifiedUser)
-router.post('/login', login)
+router.post("/", newUser);
+router.post("/activate", verifiedUser);
+router.post("/login", login);
 
-module.exports = router 
+module.exports = router;
